9ae75b2fbef5e8503a05a402c9c751f6095f06a5
JosafatGambino <kevin.hernandezg17@tectijuana.edu.mx>
2020-10-15 11:20:42 -0700

    Fibonacci series 2, 3, 5 algorithms

--- Unit_1/Fibonacci.scala
+++ Unit_1/Fibonacci.scala
@@ -1,15 +1,4 @@
-//Algoritmo 1 Versión recursiva descendente
-
-def fib(n:Int) : Int = {
-    if (n<2) {
-        return n
-    }
-    else {
-        return (fib(n-1) + fib(n-2))
-    }
-}
-
-//Algoritmo 2 Versión con fórmula explícita (6)
+//Algorithm 2 Explicit formula version
 
 def fib2(n:Int) : Double = {
     if(n<2) {
@@ -20,7 +9,7 @@ def fib2(n:Int) : Double = {
     }
 }
 
-//Algoritmo 3 Versión iterativa
+//Algorithm 3 Iterative version
 
 def fib3(n:Int) : Int = {
     var a = 0
@@ -35,20 +24,7 @@ def fib3(n:Int) : Int = {
     return a
 }
 
-//Algoritmo 4 Versión iterativa 2 variables
-
-def fib4(n:Int) : Int = {
-    var a = 0
-    var b = 1
-
-    for(i <- 1 to n){
-        b = b + a
-        a = b - a
-    }
-    return a
-}
-
-//Algoritmo 5 Versión iterativa vector
+//Algorithm 5 Iterative vector version
 
 def fib5(n:Int) : Int = {
     var vector = new Array[Int](n+2)
