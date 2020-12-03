a6bbc777cec4a328e2553d9bf57d7e738a9ca23d
pamelastephanymartinezflores <pamelastephanymartinezflores>
2020-12-02 19:54:08 -0800

    Pamela: add code analysis in Practice Decision Tree Classifier

--- practices/2. Decision Tree Classifier/CodeAnalysis_DecisionTreeClassifier.scala
+++ practices/2. Decision Tree Classifier/CodeAnalysis_DecisionTreeClassifier.scala
@@ -1,15 +1,14 @@
-// Importing this libraries is required in order to get the example done.
+//PRACTICE: CODE ANALYSIS ABOUT "DECISION TREE CLASSIFIER"
+
+//Import the following libraries
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
-
-// Start a simple spark session
 import org.apache.spark.sql.SparkSession
 
-//val spark = SparkSession.builder().getOrCreate()
-
+//An object is declared
 object DecisionTree {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -17,43 +16,45 @@ object DecisionTree {
       .appName("dtree")
       .getOrCreate()
 
-// Load the data stored in LIBSVM format as a DataFrame.
+//Load information stored in LIBSVM format for use as DataFrame
 val data = spark.read.format("libsvm").load("sample_libsvm_data.txt")
 
-// Index labels, adding metadata to the label column.
-// Fit on whole dataset to include all labels into the index.
+// Index labels, adding metadata to the label column
+// Fit on whole dataset to include all labels into the index
 val labelIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(data)
-// Automatically identify categorical features and then index them.
+
+// Automatically identify categorical features and then index them
 val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(data)
 
-// Split the data into training and test sets (30% held out for testing).
+//Split the data into training and test sets (30% held out for testing)
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
-// Train a DecisionTree model.
+//Train a Decision Tree model
 val dt = new DecisionTreeClassifier().setLabelCol("indexedLabel").setFeaturesCol("indexedFeatures")
 
-// Convert indexed labels back to original labels.
+//Convert indexed labels back to original labels
 val labelConverter = new IndexToString().setInputCol("prediction").setOutputCol("predictedLabel").setLabels(labelIndexer.labels)
 
-// Chain indexers and tree in a Pipeline.
+//Chain indexers and tree in a Pipeline
 val pipeline = new Pipeline().setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
 
-// Train the model, this also runs the indexers.
+//Train the model, this also runs the indexers
 val model = pipeline.fit(trainingData)
 
-// Make the predictions.
+//Make predictions
 val predictions = model.transform(testData)
 
-// Select example rows to display. In this case there was only 5 rows to show.
+//Select example rows to show. In this case there was only the first 5 rows.
 predictions.select("predictedLabel", "label", "features").show(5)
 
-// Select (prediction, true label)
+//Select(prediction, true label)
 val evaluator = new MulticlassClassificationEvaluator().setLabelCol("indexedLabel").setPredictionCol("prediction").setMetricName("accuracy")
-// Compute the test error.
+
+//Calculate the test error.
 val accuracy = evaluator.evaluate(predictions)
 println(s"Test Error = ${(1.0 - accuracy)}")
 
-// Show by stages the classification of the tree model
+//Show by stages the classification of the tree model
 val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
 println(s"Learned classification tree model:\n ${treeModel.toDebugString}")
 
