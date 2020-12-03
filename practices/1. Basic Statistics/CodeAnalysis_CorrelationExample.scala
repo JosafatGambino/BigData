bbfbb90448323f497a9b1ad81197710dfe31cb61
pamelastephanymartinezflores <pamelastephanymartinezflores>
2020-12-02 19:03:54 -0800

    Pamela: add code analysis in Practice Correlation

--- practices/1. Basic Statistics/CodeAnalysis_CorrelationExample.scala
+++ practices/1. Basic Statistics/CodeAnalysis_CorrelationExample.scala
@@ -1,47 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//PRACTICE: CODE ANALYSIS ABOUT "CORRELATION" 
 
-// scalastyle:off println
-package org.apache.spark.examples.ml
-
-// $example on$
+//Import libraries
 import org.apache.spark.ml.linalg.{Matrix, Vectors}
 import org.apache.spark.ml.stat.Correlation
 import org.apache.spark.sql.Row
-// $example off$
 import org.apache.spark.sql.SparkSession
 
-/**
- * An example for computing correlation matrix.
- * Run with
- * {{{
- * bin/run-example ml.CorrelationExample
- * }}}
- */
+//An object is declared
 object CorrelationExample {
 
+//Initializes a function
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("CorrelationExample")
-      .getOrCreate()
+
+    //Create a simple SparkSession
+    val spark = SparkSession.builder.appName("CorrelationExample").getOrCreate()
+    
+    //Import library
     import spark.implicits._
 
-    // $example on$
+    //Vector values are declared with the function seq for handling
     val data = Seq(
       Vectors.sparse(4, Seq((0, 1.0), (3, -2.0))),
       Vectors.dense(4.0, 5.0, 0.0, 3.0),
@@ -49,15 +26,21 @@ object CorrelationExample {
       Vectors.sparse(4, Seq((0, 9.0), (3, 1.0)))
     )
 
+    //A dataframe is created with the previously declared data and with column named "features"
     val df = data.map(Tuple1.apply).toDF("features")
+    
+    //Create correlation matrix
     val Row(coeff1: Matrix) = Correlation.corr(df, "features").head
+    //Result
     println(s"Pearson correlation matrix:\n $coeff1")
-
+    
+    //Create spearman correlation matrix
     val Row(coeff2: Matrix) = Correlation.corr(df, "features", "spearman").head
+    //Result
     println(s"Spearman correlation matrix:\n $coeff2")
-    // $example off$
-
+    
+    //Example off
     spark.stop()
   }
 }
-// scalastyle:on println
+
